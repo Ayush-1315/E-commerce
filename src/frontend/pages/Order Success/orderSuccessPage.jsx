f8f6eadf75b20add3e349a7c580d7eab834fe667
Ayush Raj <ayush.raj138@gmail.com>
2023-06-07 11:01:54 +0530

    fix: order success page

--- src/frontend/pages/Order Success/orderSuccessPage.jsx
+++ src/frontend/pages/Order Success/orderSuccessPage.jsx
@@ -1,10 +1,11 @@
 import { useUser } from "../../context/userContext"
-
+import success from "../../../images/success.gif";
 export const OrderSuccess=()=>{
     const {orders}=useUser();
     console.log(orders)
     return <>
     <h1>Order Success</h1>
-    <p>{orders[orders.length-1]?.orderId}</p>
+    <img src={success} alt="Success" style={{width: "18rem",borderRadius: "30px"}}/>
+    <p><strong>Transaction ID:</strong>{orders[orders.length-1]?.orderId}</p>
     </>
 }
